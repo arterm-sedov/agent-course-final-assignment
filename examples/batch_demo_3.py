ef87a462b340f2005cd92acbb87b6446521c921c
Arterm Sedov <arterm-sedov@users.noreply.huggingface.co>
2025-07-05 10:33:23 +0000

    Example: Batch file upload via API

---
+++ examples/batch_demo_3.py
@@ -0,0 +1,9 @@
+# Demo Python File
+# Created at: 2025-07-05 13:33:22.745588
+
+def demo_function():
+    """Demo function for batch upload example"""
+    return "Hello from batch upload!"
+
+if __name__ == "__main__":
+    print(demo_function())
